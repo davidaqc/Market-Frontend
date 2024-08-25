49b3e2a57f055cf9a23f39bdcccae9fef8100356
davidaqc <38301532+davidaqc@users.noreply.github.com>
2024-08-25 11:56:17 -0600

    Implement navbar and dropdown

--- market-frontend/src/app/shared/components/navbar/navbar.component.ts
+++ market-frontend/src/app/shared/components/navbar/navbar.component.ts
@@ -7,4 +7,78 @@ import { Component } from '@angular/core';
 })
 export class NavbarComponent {
 
+  categories = [
+    {
+      name: 'ABARROTES',
+      subcategories: [
+        "Aceites", "Artículos de papel", "Bebidas", "Cereales", "Café y té",
+        "Chocolates", "Condimentos", "Confites", "Cuidado personal", "Desechables",
+        "Enlatados", "Envolturas", "Galletas", "Granos", "Harinas", "Jaleas",
+        "Lácteos", "Libre de gluten", "Licores", "Limpieza", "Pastas", "Salsas",
+        "Siropes", "Snacks", "Sopas y consomés", "Suplementos"
+      ]
+    },
+    {
+      name: 'HOGAR',
+      subcategories: [
+        "Adornos y decoración", "Alfombras", "Baño", "Cristalería", "Plástico",
+        "Lavandería", "Cerámica", "Cocina", "Temporada lluviosa", "Maletas",
+        "Escolar y oficina", "Candelas y candelabros", "Camping e inflables",
+        "Dormitorio", "Portarretratos", "Cortinas & persianas", "Fiesta",
+        "Juguetes", "Electrodomésticos", "Bebés y damas", "Artículos deportivos",
+        "Espejos", "Equipo médico-mascarillas", "Canastas"
+      ]
+    },
+    {
+      name: 'MUEBLES',
+      subcategories: [
+        "Estantes", "Muebles De Jardín", "Oficina", "Sillas Básicas", "Bancos",
+        "Muebles De Dormitorio", "Muebles De Cocina", "Mesas Decorativas", "Muebles Plegables", "Repisas",
+        "Zapateras", "Ottoman", "Mesas Industriales", "Muebles Infantiles", "Muebles De Tv",
+        "Camas, Camarotes Y Colchones", "Sillón"
+      ]
+  },
+  {
+      name: 'FERRETERÍA',
+      subcategories: [
+        "Construcción", "Herramientas", "Iluminación", "Seguridad", "Accesorios de Ferretería",
+        "Jardinería", "Artículos Eléctricos", "Artículos para Carro", "Artículos para Motocicletas", "Acabados"
+      ]
+  },
+  {
+      name: 'MASCOTAS',
+      subcategories: [
+          "Gatos", "Perros",
+      ]
+  },
+  {
+      name: 'MES DEL NIÑO',
+      subcategories: [
+          "Subcategory 1",
+      ]
+  },
+  {
+      name: 'ELECTRODOMÉSTICOS',
+      subcategories: [
+          "Subcategory 1", "Subcategory 2"
+      ]
+  },
+  {
+      name: 'MI NEGOCIO LIMPIO',
+      subcategories: [
+          "Subcategory 1", "Subcategory 2", "Subcategory 3"
+      ]
+  }
+  ];
+
+  activeCategory: any = null;
+
+  setActiveCategory(category: any) {
+    this.activeCategory = category;
+  }
+
+  clearActiveCategory() {
+    this.activeCategory = null;
+  }
+
 }
