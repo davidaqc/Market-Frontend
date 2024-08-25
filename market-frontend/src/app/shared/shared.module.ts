876be27952142754c59b78aae74ddca4c71d7738
davidaqc <38301532+davidaqc@users.noreply.github.com>
2024-08-25 17:45:12 -0600

    Implement footer component

--- market-frontend/src/app/shared/shared.module.ts
+++ market-frontend/src/app/shared/shared.module.ts
@@ -2,20 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 
 @NgModule({
   declarations: [
     HeaderComponent,
-    NavbarComponent
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
     HeaderComponent,
-    NavbarComponent
+    NavbarComponent,
+    FooterComponent
   ]
 })
 export class SharedModule { }
