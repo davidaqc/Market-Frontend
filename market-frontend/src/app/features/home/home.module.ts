e91c35182c754829b3e3e0776791f0b4869f7907
davidaqc <38301532+davidaqc@users.noreply.github.com>
2024-08-25 14:50:11 -0600

    Implement slider (carousel) component

--- market-frontend/src/app/features/home/home.module.ts
+++ market-frontend/src/app/features/home/home.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomePageComponent } from './pages/home-page/home-page.component';
+import { SliderComponent } from './components/slider/slider.component';
 
 
 @NgModule({
   declarations: [
-    HomePageComponent
+    HomePageComponent,
+    SliderComponent
   ],
   imports: [
     CommonModule,
