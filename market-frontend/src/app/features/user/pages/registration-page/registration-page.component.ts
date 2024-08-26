221cddbd195b0bb9cd7f1726ecbfad1a9139b112
davidaqc <38301532+davidaqc@users.noreply.github.com>
2024-08-26 14:35:22 -0600

    Switching from CSS to SCSS

--- market-frontend/src/app/features/user/pages/registration-page/registration-page.component.ts
+++ market-frontend/src/app/features/user/pages/registration-page/registration-page.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-registration-page',
   templateUrl: './registration-page.component.html',
-  styleUrls: ['./registration-page.component.css']
+  styleUrls: ['./registration-page.component.scss']
 })
 export class RegistrationPageComponent {
 
