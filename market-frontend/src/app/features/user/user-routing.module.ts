f5d19d20d44707b86cff12fa19ce9e63b08ebb88
davidaqc <38301532+davidaqc@users.noreply.github.com>
2024-08-26 16:13:08 -0600

    Creating forgot-password-page component

--- market-frontend/src/app/features/user/user-routing.module.ts
+++ market-frontend/src/app/features/user/user-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RegistrationPageComponent } from './pages/registration-page/registration-page.component';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { ForgotPasswordPageComponent } from './pages/forgot-password-page/forgot-password-page.component';
 
 const routes: Routes = [
   { path: 'register', component: RegistrationPageComponent },
-  { path: 'login', component: LoginPageComponent }
+  { path: 'login', component: LoginPageComponent },
+  { path: 'forgotpassword', component: ForgotPasswordPageComponent }
 ];
 
 @NgModule({
--- market-frontend/src/app/features/user/user.module.ts
+++ market-frontend/src/app/features/user/user.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { UserRoutingModule } from './user-routing.module';
 import { RegistrationPageComponent } from './pages/registration-page/registration-page.component';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { ForgotPasswordPageComponent } from './pages/forgot-password-page/forgot-password-page.component';
 
 
 @NgModule({
   declarations: [
     RegistrationPageComponent,
-    LoginPageComponent
+    LoginPageComponent,
+    ForgotPasswordPageComponent
   ],
   imports: [
     CommonModule,
