7b944d3760ffe2ec001cfff19e9e2be09af63c99
davidaqc <38301532+davidaqc@users.noreply.github.com>
2024-08-26 16:45:04 -0600

    New info feature

---
+++ market-frontend/src/app/features/info/info.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { InfoRoutingModule } from './info-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    InfoRoutingModule
+  ]
+})
+export class InfoModule { }
