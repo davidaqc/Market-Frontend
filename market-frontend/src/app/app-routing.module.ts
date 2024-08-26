5961de104a070943e434e6e939c179ea48874cbd
davidaqc <38301532+davidaqc@users.noreply.github.com>
2024-08-26 12:01:45 -0600

    Implement Registration page

--- market-frontend/src/app/app-routing.module.ts
+++ market-frontend/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', loadChildren: () => import('./features/home/home.module').then(m => m.HomeModule) },
+  { path: 'register', loadChildren: () => import('./features/user/user.module').then(m => m.UserModule) },
 ];
 
 @NgModule({
