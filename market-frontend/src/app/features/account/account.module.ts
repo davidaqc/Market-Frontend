b1deadaf89ca15ac2f979142972f0689be96de8f
davidaqc <38301532+davidaqc@users.noreply.github.com>
2024-08-28 16:49:11 -0600

    My Account Page created

--- market-frontend/src/app/features/account/account.module.ts
+++ market-frontend/src/app/features/account/account.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AccountRoutingModule } from './account-routing.module';
+import { MyAccountComponent } from './pages/my-account/my-account.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    MyAccountComponent
+  ],
   imports: [
     CommonModule,
     AccountRoutingModule
