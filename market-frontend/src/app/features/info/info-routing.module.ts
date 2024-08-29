d69c5ae0c9e4fbfe0dfc13743b67cdd1fe2874d6
davidaqc <38301532+davidaqc@users.noreply.github.com>
2024-08-29 14:34:11 -0600

    contact-us view implemented

--- market-frontend/src/app/features/info/info-routing.module.ts
+++ market-frontend/src/app/features/info/info-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FaqPageComponent } from './pages/faq-page/faq-page.component';
+import { ContactUsPageComponent } from './pages/contact-us-page/contact-us-page.component';
 
 const routes: Routes = [
   { path: 'faq', component: FaqPageComponent },
+  { path: 'contact-us', component: ContactUsPageComponent },
 ];
 
 @NgModule({
