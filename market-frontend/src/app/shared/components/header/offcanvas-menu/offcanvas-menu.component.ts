8cfff04dc55f98abfac82720166f89bd87cb301e
davidaqc <38301532+davidaqc@users.noreply.github.com>
2024-08-28 22:59:39 -0600

    offcanvas-menu component created

---
+++ market-frontend/src/app/shared/components/header/offcanvas-menu/offcanvas-menu.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-offcanvas-menu',
+  templateUrl: './offcanvas-menu.component.html',
+  styleUrls: ['./offcanvas-menu.component.scss']
+})
+export class OffcanvasMenuComponent {
+
+}
