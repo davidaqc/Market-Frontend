8fdf2a450b0c916ef7e9b81f1d09548b5eb47876
davidaqc <38301532+davidaqc@users.noreply.github.com>
2024-08-29 17:55:14 -0600

    terms and conditions page created

---
+++ market-frontend/src/app/features/info/pages/terms-and-conditions-page/terms-and-conditions-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-terms-and-conditions-page',
+  templateUrl: './terms-and-conditions-page.component.html',
+  styleUrls: ['./terms-and-conditions-page.component.scss']
+})
+export class TermsAndConditionsPageComponent {
+
+}
