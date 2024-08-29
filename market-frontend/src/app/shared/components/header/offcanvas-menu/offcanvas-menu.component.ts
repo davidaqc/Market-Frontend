02275d6f35f1fc48fe78594a75bf459d0971c162
davidaqc <38301532+davidaqc@users.noreply.github.com>
2024-08-28 23:03:38 -0600

    offcanvas-menu view implemented

--- market-frontend/src/app/shared/components/header/offcanvas-menu/offcanvas-menu.component.ts
+++ market-frontend/src/app/shared/components/header/offcanvas-menu/offcanvas-menu.component.ts
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./offcanvas-menu.component.scss']
 })
 export class OffcanvasMenuComponent {
-
+  categories = [
+    {
+      id: 'abarrotes',
+      name: 'ABARROTES',
+      subcategories: ['Subcategory 1', 'Subcategory 2']
+    },
+    {
+      id: 'hogar',
+      name: 'HOGAR',
+      subcategories: ['Subcategory 1', 'Subcategory 2']
+    },
+    {
+      id: 'muebles',
+      name: 'MUEBLES',
+      subcategories: ['Subcategory 1', 'Subcategory 2']
+    },
+    {
+      id: 'ferreteria',
+      name: 'FERRETERÍA',
+      subcategories: ['Subcategory 1', 'Subcategory 2']
+    },
+    {
+      id: 'mascotas',
+      name: 'MASCOTAS',
+      subcategories: ['GATOS', 'PERROS']
+    }
+  ];
 }
