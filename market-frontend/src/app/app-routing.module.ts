f631e5bbf46db28610c7c402d24769b22faf9e7d
davidaqc <38301532+davidaqc@users.noreply.github.com>
2024-08-29 13:27:56 -0600

    Changing routes

--- market-frontend/src/app/app-routing.module.ts
+++ market-frontend/src/app/app-routing.module.ts
@@ -3,9 +3,9 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', loadChildren: () => import('./features/home/home.module').then(m => m.HomeModule) },
-  { path: 'customer/account', loadChildren: () => import('./features/user/user.module').then(m => m.UserModule) },
-  { path: '', loadChildren: () => import('./features/info/info.module').then(m => m.InfoModule) },
-  { path: 'customer/account', loadChildren: () => import('./features/account/account.module').then(m => m.AccountModule) },
+  { path: 'user', loadChildren: () => import('./features/user/user.module').then(m => m.UserModule) },
+  { path: 'info', loadChildren: () => import('./features/info/info.module').then(m => m.InfoModule) },
+  { path: 'account', loadChildren: () => import('./features/account/account.module').then(m => m.AccountModule) },
 ];
 
 @NgModule({
