d239d3cbb7b15628736d10ac4ad33cefa70bdfa6
davidaqc <38301532+davidaqc@users.noreply.github.com>
2024-08-29 20:38:31 -0600

    Apply navigation on the same page

--- market-frontend/src/app/features/info/pages/terms-and-conditions-page/terms-and-conditions-page.component.ts
+++ market-frontend/src/app/features/info/pages/terms-and-conditions-page/terms-and-conditions-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-terms-and-conditions-page',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./terms-and-conditions-page.component.scss']
 })
 export class TermsAndConditionsPageComponent {
+  constructor(private router: Router) {}
 
+  scrollTo(id: string): void {
+    this.router.navigate([], { fragment: id });
+  }
 }
