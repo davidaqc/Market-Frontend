d9111fc0de2ea6a964863fc5187461b97949e020
davidaqc <38301532+davidaqc@users.noreply.github.com>
2024-08-30 15:42:42 -0600

    shipping page created

--- market-frontend/src/app/features/shopping-cart/shopping-cart-routing.module.ts
+++ market-frontend/src/app/features/shopping-cart/shopping-cart-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { ReviewCartPageComponent } from './pages/review-cart-page/review-cart-page.component';
+import { ShippingComponent } from './pages/shipping/shipping.component';
 
 const routes: Routes = [
   { path: 'review', component: ReviewCartPageComponent },
+  { path: 'shipping', component: ShippingComponent },
 ];
 
 @NgModule({
--- market-frontend/src/app/features/shopping-cart/shopping-cart.module.ts
+++ market-frontend/src/app/features/shopping-cart/shopping-cart.module.ts
@@ -4,11 +4,13 @@ import { CommonModule } from '@angular/common';
 
 import { ShoppingCartRoutingModule } from './shopping-cart-routing.module';
 import { ReviewCartPageComponent } from './pages/review-cart-page/review-cart-page.component';
+import { ShippingComponent } from './pages/shipping/shipping.component';
 
 
 @NgModule({
   declarations: [
-    ReviewCartPageComponent
+    ReviewCartPageComponent,
+    ShippingComponent
   ],
   imports: [
     CommonModule,
