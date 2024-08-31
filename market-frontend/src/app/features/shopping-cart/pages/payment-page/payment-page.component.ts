309959905e775c9f6634076f64487620592462d6
davidaqc <38301532+davidaqc@users.noreply.github.com>
2024-08-30 20:16:11 -0600

    payment page implemented

--- market-frontend/src/app/features/shopping-cart/pages/payment-page/payment-page.component.ts
+++ market-frontend/src/app/features/shopping-cart/pages/payment-page/payment-page.component.ts
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class PaymentPageComponent {
 
+  currentStep: 'shipping' | 'payment' = 'payment';
+
+  orderSummary = {
+    subtotal: 192000,
+    shipping: 0,
+    total: 192000,
+    itemCount: 3
+  };
+
+  shippingInfo = {
+    name: 'David Quesada',
+    address1: 'a, a, a',
+    cityState: 'CARTAGO, Cartago',
+    country: 'Costa Rica',
+    phone: '71197531',
+    coordinates: {
+      lat: 10.0159394,
+      lng: -84.2141700999999
+    }
+  };
 }
