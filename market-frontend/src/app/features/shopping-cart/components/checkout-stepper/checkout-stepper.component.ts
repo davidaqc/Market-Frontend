0d82b80ee76241f4585f7ce07c361e6cc7f95afa
davidaqc <38301532+davidaqc@users.noreply.github.com>
2024-08-30 18:57:47 -0600

    checkout-stepper created

---
+++ market-frontend/src/app/features/shopping-cart/components/checkout-stepper/checkout-stepper.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-checkout-stepper',
+  templateUrl: './checkout-stepper.component.html',
+  styleUrls: ['./checkout-stepper.component.scss']
+})
+export class CheckoutStepperComponent {
+
+}
--- market-frontend/src/app/features/shopping-cart/shopping-cart.module.ts
+++ market-frontend/src/app/features/shopping-cart/shopping-cart.module.ts
@@ -5,12 +5,14 @@ import { CommonModule } from '@angular/common';
 import { ShoppingCartRoutingModule } from './shopping-cart-routing.module';
 import { ReviewCartPageComponent } from './pages/review-cart-page/review-cart-page.component';
 import { ShippingComponent } from './pages/shipping/shipping.component';
+import { CheckoutStepperComponent } from './components/checkout-stepper/checkout-stepper.component';
 
 
 @NgModule({
   declarations: [
     ReviewCartPageComponent,
-    ShippingComponent
+    ShippingComponent,
+    CheckoutStepperComponent
   ],
   imports: [
     CommonModule,
