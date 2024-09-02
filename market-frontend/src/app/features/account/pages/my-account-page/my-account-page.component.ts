682c1305ff6f982050cdd6ee16d6da3acdc39745
davidaqc <38301532+davidaqc@users.noreply.github.com>
2024-09-02 10:07:47 -0600

    my-account-design implemented

--- market-frontend/src/app/features/account/pages/my-account-page/my-account-page.component.ts
+++ market-frontend/src/app/features/account/pages/my-account-page/my-account-page.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./my-account-page.component.scss']
 })
 export class MyAccountPageComponent {
+  contactInfo = {
+    name: 'David Quesada',
+    email: 'davidqc05@gmail.com'
+  };
 
+  newsletterMessage = 'No estás suscrito a nuestro boletín de noticias.';
+
+  paymentAddressMessage = 'No has establecido una dirección de facturación predeterminada.';
+
+  shippingAddress = {
+    name: 'David Quesada',
+    canton: 'Quebradilla',
+    address: 'Un costado oeste de la iglesia de Bermejo',
+    province: 'Cartago',
+    country: 'Costa Rica',
+    phone: '71197531'
+  };
+
+  recentReviews = [
+    { productName: 'Aceite aguacate Mueloliva 1lt', rating: 5 }
+  ];
 }
