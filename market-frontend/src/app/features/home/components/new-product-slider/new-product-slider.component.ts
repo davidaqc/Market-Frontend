9ff15f940c81f97d9d4189813197a5f360e65248
davidaqc <38301532+davidaqc@users.noreply.github.com>
2024-09-01 19:39:58 -0600

    New product slider implemented

--- market-frontend/src/app/features/home/components/new-product-slider/new-product-slider.component.ts
+++ market-frontend/src/app/features/home/components/new-product-slider/new-product-slider.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./new-product-slider.component.scss']
 })
 export class NewProductSliderComponent {
-
+  products = [
+    { name: 'Anteojos natación edad 14+ Bestway', image: 'assets/images/anteojos_natacion.jpg', price: 2000 },
+    { name: 'Gaseosa minilata Coca Cola 237ml 12unds', image: 'assets/images/bebida_avena.jpg', price: 14500 },
+    { name: 'Hamaca asiento cojín Polialgodón 100x130cm', image: 'assets/images/camarote_individual.jpg', price: 8000 },
+    { name: 'Bebida avena Natur-a 946ml', image: 'assets/images/gaseosa_minilata_coca_cola.jpg', price: 3000 },
+    { name: 'Hamaca tela rayas Algodón 80x200x265cm', image: 'assets/images/hamaca_asiento_cojin.jpg', price: 9000 },
+    { name: 'Camarote individual metálico 200x107x149cm', image: 'assets/images/hamaca_tela_rayas.jpg', price: 22000 },
+    { name: 'Hielera con agarradera Plástica Igloo 47lt', image: 'assets/images/hielera_agarradera.jpg', price: 5500 }
+  ];
 }
